6555b7a4aeb8f8b4e817d52a052e14958ccd8078
horooga <saintnurlan@gmail.com>
2025-03-14 00:13:29 +0300

    + to minecraft pic

--- image_procs_n_raytrace.rs
+++ image_procs_n_raytrace.rs
@@ -10,7 +10,6 @@ pub use std::str;
 pub use types_n_convs::*;
 pub use Vec;
 
-
 pub const BAYER_8X8: [[u8; 8]; 8] = [
     [0, 32, 8, 40, 2, 34, 10, 42],
     [48, 16, 56, 24, 50, 18, 58, 26],
@@ -22,7 +21,7 @@ pub const BAYER_8X8: [[u8; 8]; 8] = [
     [63, 31, 55, 23, 61, 29, 53, 21],
 ];
 
-pub fn get_n_index(v: String) -> usize {
+fn get_n_index(v: String) -> usize {
     let mut c: i32 = 0;
     for i in 0..v.len() {
         if v.chars().nth(i).unwrap() == '/' {
@@ -34,7 +33,7 @@ pub fn get_n_index(v: String) -> usize {
     return 0;
 }
 
-pub fn get_v_indexes(line: Vec<String>) -> Vec<usize> {
+fn get_v_indexes(line: Vec<String>) -> Vec<usize> {
     let mut output: Vec<usize> = vec![];
     for i in 1..line.len() {
         let v: String = line[i].clone();
@@ -48,14 +47,14 @@ pub fn get_v_indexes(line: Vec<String>) -> Vec<usize> {
     return output;
 }
 
-pub fn rgb_dist(one: [i32; 3], two: [i32; 3]) -> u16 {
+fn rgb_dist(one: [i32; 3], two: [i32; 3]) -> u16 {
     ((i32_sub(one[0], two[0]).pow(2)
         + i32_sub(one[1], two[1]).pow(2)
         + i32_sub(one[2], two[2]).pow(2)) as f32)
         .sqrt() as u16
 }
 
-pub fn closest_color(colors_from: Vec<[i32; 3]>, color_to: [i32; 3]) -> Rgb<u8> {
+fn closest_color(colors_from: Vec<[i32; 3]>, color_to: [i32; 3]) -> Rgb<u8> {
     let mut mindist: u16 = 65535;
     let mut dist: u16;
     let mut color_index: usize = 0;
@@ -100,18 +99,27 @@ pub fn closest_color(colors_from: Vec<[i32; 3]>, color_to: [i32; 3]) -> Rgb<u8> 
     ])
 }
 
-pub fn closest_index(max_index: usize, val_to: f32) -> usize {
+fn closest_index(max_index: usize, val_to: f32) -> usize {
     let mut mindist: f32 = 65535.0;
     let mut dist: f32;
     let mut index: usize = 0;
-    for ci in 0..max_index {
-        dist = (val_to - ci as f32).abs();
+    for i in 0..max_index {
+        dist = (val_to - i as f32).abs();
         if dist < mindist {
             mindist = dist;
-            index = ci;
+            index = i;
         }
     }
     return index;
+}
+
+fn measure_equal(one: Rgb<u8>, two: Rgb<u8>, measure: i32) -> bool {
+    for i in 0..3 {
+        if (two[i as usize] as i32 - one[i as usize] as i32).abs() > measure {
+            return false;
+        }
+    }
+    return true;
 }
 
 pub fn ord_bayer_dithering(
@@ -123,7 +131,8 @@ pub fn ord_bayer_dithering(
     m: f32,
 ) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
     let (x, y) = img.dimensions();
-    let mut nimg: ImageBuffer<Rgb<u8>, Vec<u8>> = RgbImage::new(x / pixel_size, y / pixel_size);
+    let mut nimg: ImageBuffer<Rgb<u8>, Vec<u8>> =
+        RgbImage::new(x / pixel_size + 1, y / pixel_size + 1);
     let mut ic = 0;
     let mut jc = 0;
     for i in (0..y).step_by(pixel_size as usize) {
@@ -154,7 +163,8 @@ pub fn twod_errprop_dithering(
     m: f32,
 ) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
     let (x, y) = img.dimensions();
-    let mut nimg: ImageBuffer<Rgb<u8>, Vec<u8>> = RgbImage::new(x / pixel_size, y / pixel_size);
+    let mut nimg: ImageBuffer<Rgb<u8>, Vec<u8>> =
+        RgbImage::new(x / pixel_size + 1, y / pixel_size + 1);
     let mut ic = 0;
     let mut jc;
     let mut err: [i32; 3] = [0; 3];
@@ -171,9 +181,12 @@ pub fn twod_errprop_dithering(
             color = closest_color(
                 pal.clone(),
                 [
-                    (curr_pixel[0] as f32 / d) as i32 + ((err[0] + err_line[0][0]) as f32 * m) as i32,
-                    (curr_pixel[1] as f32 / d) as i32 + ((err[1] + err_line[0][1]) as f32 * m) as i32,
-                    (curr_pixel[2] as f32 / d) as i32 + ((err[2] + err_line[0][2]) as f32 * m) as i32,
+                    (curr_pixel[0] as f32 / d) as i32
+                        + ((err[0] + err_line[0][0]) as f32 * m) as i32,
+                    (curr_pixel[1] as f32 / d) as i32
+                        + ((err[1] + err_line[0][1]) as f32 * m) as i32,
+                    (curr_pixel[2] as f32 / d) as i32
+                        + ((err[2] + err_line[0][2]) as f32 * m) as i32,
                 ],
             );
             nimg.put_pixel(jc, ic, color);
@@ -181,7 +194,7 @@ pub fn twod_errprop_dithering(
                 curr_pixel[0] as i32 - color[0] as i32,
                 curr_pixel[1] as i32 - color[1] as i32,
                 curr_pixel[2] as i32 - color[2] as i32,
-            ]; 
+            ];
             err_line[jc as usize] = [
                 upper_pixel[0] as i32 - color[0] as i32,
                 upper_pixel[1] as i32 - color[1] as i32,
@@ -226,33 +239,57 @@ pub fn ascii_ord_bayer_dithering(
     Ok(())
 }
 
-pub fn rec_to_quad(img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+pub fn to_colors(img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>) -> Vec<[i32; 3]> {
     let (x, y) = img.dimensions();
-    let size: u32 = cmp::min(x, y);
-    let offset: u32 = (x - size) / 2;
-    let mut nimg: ImageBuffer<Rgb<u8>, Vec<u8>> = RgbImage::new(size, size);
-    for i in 0..size {
-        for j in 0..size {
-            nimg.put_pixel(j, i, *img.get_pixel(j + offset, i));
+    let mut colors: Vec<[i32; 3]> = vec![];
+    let mut pixel: [u8; 3];
+    let mut color: [i32; 3];
+    for i in 0..y {
+        for j in 0..x {
+            pixel = img.get_pixel(j, i).0;
+            color = [pixel[0] as i32, pixel[1] as i32, pixel[2] as i32];
+            if !(colors.contains(&color)) {
+                colors.push(color);
+            }
         }
     }
-    nimg
+    colors
 }
 
-pub fn edit_color(img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, colors: Vec<Rgb<u8>>) {
+pub fn to_n_val_channels(img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, mut n: u8) {
+    let (x, y) = img.dimensions();
+    let mut pixel_val: [u8; 3];
+    n = 255 / n;
+    for i in 0..y {
+        for j in 0..x {
+            pixel_val = img.get_pixel(j, i).0;
+            img.put_pixel(
+                j,
+                i,
+                Rgb([
+                    (pixel_val[0] as f32 / n as f32) as u8 * n,
+                    (pixel_val[1] as f32 / n as f32) as u8 * n,
+                    (pixel_val[2] as f32 / n as f32) as u8 * n,
+                ]),
+            )
+        }
+    }
+}
+
+pub fn edit_color(img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, colors: Vec<Rgb<u8>>, measure: i32) {
     let (x, y) = img.dimensions();
     for i in 0..y {
         for j in 0..x {
             for k in (0..colors.len()).step_by(2) {
-                if colors[k] == *img.get_pixel(j, i) {
-                    img.put_pixel(j, i, colors[k+1]);
+                if measure_equal(colors[k], *img.get_pixel(j, i), measure) {
+                    img.put_pixel(j, i, colors[k + 1]);
                 }
             }
         }
     }
 }
 
-pub fn upscale(img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, k: u32) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+pub fn upscale(img: ImageBuffer<Rgb<u8>, Vec<u8>>, k: u32) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
     let (x, y) = img.dimensions();
     let mut nimg: ImageBuffer<Rgb<u8>, Vec<u8>> = RgbImage::new(x * k, y * k);
     for i in 0..(y * k) {
@@ -263,10 +300,26 @@ pub fn upscale(img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, k: u32) -> ImageBuffer<R
     nimg
 }
 
+pub fn downscale(img: ImageBuffer<Rgb<u8>, Vec<u8>>, k: u32) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+    let (x, y) = img.dimensions();
+    let mut nimg: ImageBuffer<Rgb<u8>, Vec<u8>> = RgbImage::new(x / k + 1, y / k + 1);
+    let mut ic: u32 = 0;
+    let mut jc: u32 = 0;
+    for i in (0..y).step_by(k as usize) {
+        jc = 0;
+        for j in (0..x).step_by(k as usize) {
+            nimg.put_pixel(jc, ic, *img.get_pixel(j, i));
+            jc += 1;
+        }
+        ic += 1;
+    }
+    nimg
+}
+
 pub fn pinkize(img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>) {
     let (x, y) = img.dimensions();
     let mut output_img: ImageBuffer<Rgb<u8>, Vec<u8>> = RgbImage::new(x, y);
-    for i in 0..y { 
+    for i in 0..y {
         for j in 0..x {
             let pixel: Rgb<u8> = *img.get_pixel(j, i);
             let c_max: u8 = cmp::max(pixel[2], cmp::max(pixel[0], pixel[1]));
@@ -336,6 +389,23 @@ pub fn to_cpp_arr(img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, s: i32, f: bool) {
         }
     }
     print!("}}");
+}
+
+pub fn to_mc_pic(img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, width: u32, frame_color: Rgb<u8>) {
+    let (x, y) = img.dimensions();
+
+    for i in 0..(y - 1) {
+        for w in 0..width {
+            img.put_pixel(0 + w, i, frame_color);
+            img.put_pixel(x - 1 - w, i, frame_color);
+        }
+    }
+    for j in 0..(x - 1) {
+        for w in 0..width {
+            img.put_pixel(j, 0 + w, frame_color);
+            img.put_pixel(j, y - 1 - w, frame_color);
+        }
+    }
 }
 
 pub fn obj_import(name: &str, pos: [f32; 3], rfty: f32) -> std::io::Result<Vec<Object>> {
--- main.rs
+++ main.rs
@@ -4,25 +4,19 @@ use image_procs_n_raytrace::*;
 fn main() {
     //let img: ImageBuffer<Rgb<u8>, Vec<u8>> = raytrace(vec![], [256, 256], Xyz::new([0.0, 0.0, 0.0]), 0, Rgb([0, 0, 0]));
 
-    let mut img: ImageBuffer<Rgb<u8>, Vec<u8>> = open_img("image.png").unwrap();
+    let mut img: ImageBuffer<Rgb<u8>, Vec<u8>> = open_img("image.jpg").unwrap();
+    img = downscale(img, 8);
     let mut rimg: &mut ImageBuffer<Rgb<u8>, Vec<u8>> = &mut img;
 
-    let pal: Vec<[i32; 3]> = vec![
-        [20, 20, 20],
-        [55, 34, 55],
-        [75, 50, 75],
-        [100, 66, 100],
-        //[180, 120, 180],
-        [255, 180, 255],
-    ];
-    img = rec_to_quad(rimg);
+    to_n_val_channels(rimg, 6);
+    let colors: Vec<[i32; 3]> = to_colors(rimg);
+    //print!("{:?}", colors);
+
+    img = ord_bayer_dithering(img, colors, BAYER_8X8, 2, 1.0, 0.0);
+
     rimg = &mut img;
-    edit_color(rimg, vec![Rgb([0, 0, 0]), Rgb([80, 30, 80])]);
-    //img = ord_bayer_dithering(img, pal, BAYER_8X8, 4, 1.7, 70.0);
-    img = twod_errprop_dithering(img, pal, 4, 1.8, 0.7);
-    rimg = &mut img;
-    colorize(rimg, Rgb([255, 175, 255]));
-    bright(rimg, 1.5);
-    img = upscale(rimg, 2);
+    bright(rimg, 1.3);
+    to_mc_pic(rimg, 3, Rgb([60, 20, 20]));
+
     let _ = save_img(img, "res.jpg");
 }
